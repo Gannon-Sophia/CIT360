4f17c42e72ab995f5214b745b19ce1a266583b9a
Sophie <mslgan15@gmail.com>
2018-03-09 19:54:40 -0600

    Update JUnitCountTest.java

--- JUnitCountTest.java
+++ JUnitCountTest.java
@@ -9,7 +9,7 @@ class JUnitCountTest {
 	@Test
 	void test() {
 		JUnitSample test = new JUnitSample();
-		int output = test.countA("alphabet");
+		int output = test.countJ("alphabet");
 		assertEquals(2, output);
 	}
 	
